370cc21bf54c67a7fbe8a9a644945482c45c6a5e
seulgi <asd0927122@gmail.com>
2023-11-06 13:48:22 +0900

    frist commit

---
+++ src/main/java/com/example/livingbymyselfserver/LivingByMyselfServerApplication.java
@@ -0,0 +1,13 @@
+package com.example.livingbymyselfserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivingByMyselfServerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(LivingByMyselfServerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/livingbymyselfserver/LivingByMyselfServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.livingbymyselfserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivingByMyselfServerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
