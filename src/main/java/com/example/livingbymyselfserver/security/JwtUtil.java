d25817bafaeb62ed9c3d0051c1ad7b4e57f487e5
seulgi <asd0927122@gmail.com>
2023-11-08 00:56:56 +0900

    Add,Refactor(ksg) : Redis 적용하여 Atk,Rtk 생성 및 Atk 재발급API 작성 #1

--- src/main/java/com/example/livingbymyselfserver/security/JwtUtil.java
+++ src/main/java/com/example/livingbymyselfserver/security/JwtUtil.java
@@ -1,5 +1,8 @@
 package com.example.livingbymyselfserver.security;
 
+import com.example.livingbymyselfserver.common.RedisUtil;
+import com.example.livingbymyselfserver.user.User;
+import com.example.livingbymyselfserver.user.UserRoleEnum;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
@@ -13,6 +16,7 @@ import jakarta.servlet.http.HttpServletRequest;
 import java.security.Key;
 import java.util.Base64;
 import java.util.Date;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
@@ -20,14 +24,23 @@ import org.springframework.util.StringUtils;
 
 @Slf4j(topic = "JwtUtil")
 @Component
+
 public class JwtUtil {
+  private final RedisUtil redisUtil;
+
+  public JwtUtil(RedisUtil redisUtil) {
+    this.redisUtil = redisUtil;
+  }
+
   // Header KEY 값
   public static final String AUTHORIZATION_HEADER = "Authorization";
   // 사용자 권한 값의 KEY
   public static final String AUTHORIZATION_KEY = "auth";
   // Token 식별자
   public static final String BEARER_PREFIX = "Bearer ";
+
   // 토큰 만료시간
+  public static final long REFRESH_TOKEN_TIME = 7 * 24 * 60 * 60 * 1000L; // 7일
   private final long TOKEN_TIME = 60 * 60 * 1000L; // 60분
 
   @Value("${jwt.secret.key}") // Base64 Encoded SecretKey
@@ -50,18 +63,33 @@ public class JwtUtil {
     return null;
   }
 
-  // 토큰 생성
-  public String createToken(String username) {
+  // access token 토큰 생성
+  public String createToken(String username, UserRoleEnum role) {
     Date date = new Date();
 
     return BEARER_PREFIX +
         Jwts.builder()
             .setSubject(username) // 사용자 식별자값(ID)
+            .claim(AUTHORIZATION_KEY, role)  //사용자 권한
             .setExpiration(new Date(date.getTime() + TOKEN_TIME)) // 만료 시간
             .setIssuedAt(date) // 발급일
             .signWith(key, signatureAlgorithm) // 암호화 알고리즘
             .compact();
   }
+
+
+  // refresh token 생성
+  public String createRefreshToken(String username, UserRoleEnum role) {
+    Date now = new Date();
+
+    return BEARER_PREFIX + Jwts.builder()
+        .setSubject(username) // 사용자 식별자값(ID)
+        .claim(AUTHORIZATION_KEY, role)  //사용자 권한
+        .setExpiration(new Date(now.getTime() + REFRESH_TOKEN_TIME))  //만료시간
+        .signWith(key, signatureAlgorithm)
+        .compact();
+  }
+
 
   // JWT 검증
   public boolean validateToken(String token) {
@@ -80,9 +108,23 @@ public class JwtUtil {
     return false;
   }
 
+  // refreshToken 확인
+  public boolean checkRefreshToken(String username, String token) {
+    String storedToken = redisUtil.getRefreshToken(username);
+    return storedToken !=null && storedToken.equals(token);
+  }
+
+
+
 
   // 토큰에서 사용자 정보 가져오기
   public Claims getUserInfoFromToken(String token) {
     return Jwts.parserBuilder().setSigningKey(key).build().parseClaimsJws(token).getBody();
   }
+
+  public String getUsernameFromToken(String token) {
+    Claims claims = getUserInfoFromToken(token);
+    return claims.get("username", String.class);
+  }
+
 }
--- src/main/java/com/example/livingbymyselfserver/user/UserController.java
+++ src/main/java/com/example/livingbymyselfserver/user/UserController.java
@@ -1,15 +1,23 @@
 package com.example.livingbymyselfserver.user;
 
 import com.example.livingbymyselfserver.common.ApiResponseDto;
+import com.example.livingbymyselfserver.security.JwtUtil;
+import com.example.livingbymyselfserver.security.TokenResponseDto;
+import com.example.livingbymyselfserver.security.UserDetailsImpl;
 import com.example.livingbymyselfserver.user.dto.LoginRequestDto;
 import com.example.livingbymyselfserver.user.dto.SignupRequestDto;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.nimbusds.oauth2.sdk.TokenResponse;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -33,9 +41,18 @@ public class UserController {
 
   @Operation(summary = "로그인")
   @PostMapping("/login")
-  public ResponseEntity<ApiResponseDto> login(@RequestBody LoginRequestDto requestDto, HttpServletResponse response) {
-    ApiResponseDto result = userService.login(requestDto, response);
+  public ResponseEntity<TokenResponseDto> login(@RequestBody LoginRequestDto requestDto) {
+    TokenResponseDto result = userService.login(requestDto);
     return ResponseEntity.status(HttpStatus.OK).body(result);
   }
 
+
+  // 변경 필요
+  @Operation(summary = "엑세스 토큰 재발급")
+  @GetMapping("/reissue")
+  public TokenResponseDto reissue(
+      @AuthenticationPrincipal UserDetailsImpl userDetails, HttpServletRequest request) {
+    TokenResponseDto result = userService.reissue(userDetails.getUser(), request);
+    return result;
+  }
 }
