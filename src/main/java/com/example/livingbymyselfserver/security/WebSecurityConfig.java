d08068877d29f2992ad574518570c40611f2c12f
SeungBum Baek <wer06099@naver.com>
2023-11-18 11:07:09 +0900

    Add(BSB) : WebSecurityConfig 프리플라이트 관련 설정 추가 #6

--- src/main/java/com/example/livingbymyselfserver/security/WebSecurityConfig.java
+++ src/main/java/com/example/livingbymyselfserver/security/WebSecurityConfig.java
@@ -11,6 +11,9 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity // Spring Security 지원을 가능하게 함
@@ -63,6 +66,21 @@ public class WebSecurityConfig {
     http.addFilterBefore(jwtAuthorizationFilter(), UsernamePasswordAuthenticationFilter.class);
 
     return http.build();
+  }
+
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.addAllowedOrigin("*"); // 프론트엔드 도메인 설정
+    configuration.addAllowedMethod("*"); // 모든 HTTP 메서드 허용
+    configuration.addAllowedHeader("*"); // 모든 헤더 허용
+    configuration.setAllowCredentials(true); // credentials 허용
+    configuration.setMaxAge(3600L); // 3600초(1시간) 동안 preflight 요청 결과를 캐시
+
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+
+    return source;
   }
 
 }
